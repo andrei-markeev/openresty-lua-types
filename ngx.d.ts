828a06d6f5b9c6a1f4c12c4d39fcb98ddf63e329
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2022-04-16 16:38:23 +0300

    add cjson to reference path

--- ngx.d.ts
+++ ngx.d.ts
@@ -7,6 +7,7 @@
 /// <reference path="./ngx.request.d.ts" />
 /// <reference path="./ngx.shared.d.ts" />
 /// <reference path="./ngx.var.d.ts" />
+/// <reference path="./cjson.d.ts" />
 
 declare const ngx: NgxModule;
 
